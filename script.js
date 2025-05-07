fb1dcf7c964a92261bed399b2e70e50f07d6364d
danichagas <chagasdaniel788@gmail.com>
2025-05-07 00:36:27 -0300

    feat: calculating the total value

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const expense = document.getElementById('expense')
 const category = document.getElementById('category')
 
 const expenseList = document.querySelector('ul')
+const expensesTotal = document.querySelector('aside header h2')
 const expensesQuantity = document.querySelector('aside header p span')
 
 amount.oninput = () => {
@@ -84,9 +85,20 @@ function updateTotals() {
         expensesQuantity.textContent = `${items.length} ${items.length > 1 ? 'despesas' : 'despesa'}`
 
         let total = 0
-        for (let item = 0 ; item < items.length; item++) {
-            const itemAmount = items[item].querySelector('expense-amount')
+        for(let item = 0 ; item < items.length; item++) {
+            const itemAmount = items[item].querySelector('.expense-amount')
+
+            let value= itemAmount.textContent.replace(/[^\d]/g, '').replace(',','.')
+            value = parseFloat(value)
+
+            if(isNaN(value)) {
+                return alert('Não foi possìvel calcular o total. O valor não parece ser um número')
+            }
+
+            total += Number(value)
         }
+
+        expensesTotal.textContent = total
     } catch (error) {
         console.log(error)
         alert('Não foi possível atualizar as despesas')
